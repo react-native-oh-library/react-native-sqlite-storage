d44d1a6e7d792f4832e4c843638581f8ba730a07
quweifan <345827250@qq.com>
2024-05-29 11:48:56 +0800

    鸿蒙代码query方法回调方法加如延时器

--- NativeSQLitePlugin.ts
+++ NativeSQLitePlugin.ts
@@ -2,13 +2,19 @@ import type {TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
 import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
-  DEBUG(isDebug: boolean): void;
-  enablePromise(enablePromise: boolean): void;
-  openDatabase(dbname:string,dbVersion:string,dbDisplayname:string,dbSize:number,success?: () => void,error?: (e: Object) => void,): Object;
-  deleteDatabase(dbname: string, success?: () => void, error?: (err: Object) => void): void;
-  executeSql(store:Object,statement: string, params?: any[], success?: (s?: Object) => void, error?: (e: Object) => void): Promise<void>;
-  // transaction(scope: (tx: Transaction) => void,error?: (e: Object) => void,success?: () => void,): void;
-  close(store:Object,success: () => void, error: (err: Object) => void): void;
+  
+  open(openargs:Object,opensuccesscb:() => void,openerrorcb:(e:Object) => void):void;
+
+  close(closeargs:Object,mysuccess:(t:Object,r:Object) => void,myerror:(e:Object) => void):void;
+
+  attach(attachargs:Object,mysuccess:(t:Object,r:Object) => void,myerror:(e:Object) => void):void;
+
+  backgroundExecuteSqlBatch(args:Object,mysuccess:(result:Object) => void,myerror:(e:Object) => void):void;
+
+  echoStringValue(openargs:Object,mysuccess:(testValue:Object) => void,myerror:(e:Object) => void):void;
+
+  delete(args:Object,mysuccess:(r:Object) => void,myerror:(e:Object) => void):void;
+
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('SQLitePlugin');
